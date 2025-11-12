62adc18415edf6d0b66c99fa46d1096c41990b3d
AzureAD\AnhNguyenCong <anh.nguyencong@nashtechglobal.com>
2025-11-12 22:37:39 +0700

    Add bouncing dots spinner

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DialogComponent } from '@app/shared/components/dialog/dialog';
+import { DialogComponent } from '@shared/components/dialog/dialog';
 
 @Component({
   selector: 'app-root',
---
+++ frontend/src/app/shared/components/bouncing-dots-spinner/bouncing-dots-spinner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bouncing-dots-spinner',
+  imports: [],
+  templateUrl: './bouncing-dots-spinner.html',
+  styleUrl: './bouncing-dots-spinner.scss'
+})
+export class BouncingDotsSpinner {
+
+}
